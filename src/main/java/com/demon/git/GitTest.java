5c5d9a5f52b835594310190535fb5d337474f39c
LiSong <LiSong@qq.com>
2021-08-13 20:05:02 +0800

    first commit

---
+++ src/main/java/com/demon/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.demon.git;
+
+/**
+ * @author song
+ * @create 2021-08-13 19:57
+ */
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+}
